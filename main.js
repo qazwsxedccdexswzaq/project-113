deafa8960dc9ee21923698c250cc7a23a82dde47
qazwsxedccdexswzaq <96393413+qazwsxedccdexswzaq@users.noreply.github.com>
2022-11-26 22:19:51 -1000

    Add files via upload

--- main.js
+++ main.js
@@ -4,9 +4,33 @@ function preload(){
 function setup() {
     canvas = createCanvas(400,400);
     canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(400,400);
+    video.hide();
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+
 }
 
 function draw(){
+    image(video,0,0,400,400);
+}
+
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        console.log("nose x is ="+results[0].pose.nose.x);
+        console.log("nose y is ="+results[0].pose.nose.y);
+    }
+}
+
+function modelLoaded()
+{
+    console.log("poseNet is Initialized")
 }
 
 function take_snapshot(){
